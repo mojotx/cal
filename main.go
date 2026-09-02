eb849009260b0a7f566ef731eb3e4962da893110
Michael Jarvis <michael@jarvis.com>
2026-09-01 20:04:14 -0500

    Do bounds checking on int64->int conversions

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"time"
@@ -8,6 +10,17 @@ import (
 	"github.com/fatih/color"
 	"github.com/mojotx/cal/pkg/calendar"
 )
+
+func parseIntArg(s string) (int, error) {
+	value, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	if value < math.MinInt || value > math.MaxInt {
+		return 0, fmt.Errorf("value %q overflows int", s)
+	}
+	return int(value), nil
+}
 
 func main() {
 	switch len(os.Args) {
@@ -19,41 +32,41 @@ func main() {
 
 	// One argument is a year
 	case 2:
-		year, err := strconv.ParseInt(os.Args[1], 10, 64)
+		year, err := parseIntArg(os.Args[1])
 		if err != nil {
 			color.Red("error parsing year: %s", err.Error())
 			os.Exit(1)
 		}
-		if err := calendar.ValidateYear(int(year)); err != nil {
+		if err := calendar.ValidateYear(year); err != nil {
 			color.Red("error: %s", err.Error())
 			os.Exit(1)
 		}
-		if err := calendar.DumpYear(int(year)); err != nil {
+		if err := calendar.DumpYear(year); err != nil {
 			color.Red("error: %s", err.Error())
 			os.Exit(1)
 		}
 
 	// Two arguments: month and year
 	case 3:
-		month, err := strconv.ParseInt(os.Args[1], 10, 64)
+		month, err := parseIntArg(os.Args[1])
 		if err != nil {
 			color.Red("error parsing month: %s", err.Error())
 			os.Exit(1)
 		}
-		year, err := strconv.ParseInt(os.Args[2], 10, 64)
+		year, err := parseIntArg(os.Args[2])
 		if err != nil {
 			color.Red("error parsing year: %s", err.Error())
 			os.Exit(1)
 		}
-		if err := calendar.ValidateMonth(int(month)); err != nil {
+		if err := calendar.ValidateMonth(month); err != nil {
 			color.Red("error: %s", err.Error())
 			os.Exit(1)
 		}
-		if err := calendar.ValidateYear(int(year)); err != nil {
+		if err := calendar.ValidateYear(year); err != nil {
 			color.Red("error: %s", err.Error())
 			os.Exit(1)
 		}
-		calendar.DumpMonth(time.Month(month), int(year))
+		calendar.DumpMonth(time.Month(month), year)
 
 	default:
 		color.Red("usage: %s [month] [year]", os.Args[0])
